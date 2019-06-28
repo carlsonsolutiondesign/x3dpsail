2384c87a6813df263fd416497665cc77e5ea94b5
John Carlson <yottzumm@gmail.com>
2019-06-28 12:34:16 -0500

    tried to fix classpath issues

--- x3dpsail.py
+++ x3dpsail.py
@@ -1,5 +1,5 @@
 import jnius_config
-jnius_config.set_classpath('c:/Users/coderextreme/x3dpsail/X3DJSAIL.3.3.full.jar', '../../../../stylesheets/java/jars/X3DJSAIL.3.3.full.jar', './X3DJSAIL.3.3.full.jar')
+jnius_config.set_classpath('.', 'c:/Users/coderextreme/x3dpsail/X3DJSAIL.3.3.full.jar', '../../../../stylesheets/java/jars/X3DJSAIL.3.3.full.jar', './X3DJSAIL.3.3.full.jar')
 from jnius import autoclass
 CommentsBlock = autoclass('org.web3d.x3d.jsail.Core.CommentsBlock')
 Anchor = autoclass('org.web3d.x3d.jsail.Networking.AnchorObject')
